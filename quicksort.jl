47ed3d7c1860632d7b0a4ecfcbe26a45bf4008bf
George Thomas <georgetamayothomas@gmail.com>
2015-11-09 11:37:23 +0000

    Quick sort comparisons

--- quicksort.jl
+++ quicksort.jl
@@ -14,19 +14,21 @@ function quicksort_ip!(A::Array{Int})
   quicksort_ip!(A, 1, length(A))
 end
 
-function quicksort_ip!(A::Array{Int}, lo::Int, hi::Int)
-  function partition!(A::Array{Int}, lo::Int, hi::Int)
-    pivot = A[hi]
-    i = lo
-    for j = i:hi-1
-      if A[j] ≤ pivot
-        A[i], A[j] = A[j], A[i]
-        i += 1
-      end
+
+function partition!(A::Array{Int}, lo::Int, hi::Int)
+  pivot = A[hi]
+  i = lo
+  for j = i:hi-1
+    if A[j] ≤ pivot
+      A[i], A[j] = A[j], A[i]
+      i += 1
     end
-    A[i], A[hi] = A[hi], A[i]
-    return i
   end
+  A[i], A[hi] = A[hi], A[i]
+  return i
+end
+
+function quicksort_ip!(A::Array{Int}, lo::Int, hi::Int)
   if lo < hi
     p = partition!(A, lo, hi)
     quicksort_ip!(A, lo, p-1)
@@ -43,6 +45,16 @@ println("Using quicksort in place")
 quicksort_ip!([2,1,3])
 nums = rand(1:1_000_000, 1_000_000)
 @time quicksort_ip!(nums)
+
+println("Using sort!")
+sort!([2,1,3])
+nums = rand(1:1_000_000, 1_000_000)
+@time sort!(nums)
+
+println("Using Julia quicksort")
+sort!([2,1,3]; alg=QuickSort)
+nums = rand(1:1_000_000, 1_000_000)
+@time sort!(nums; alg=QuickSort)
 
 # Using quicksort alloc
 #  11.274339 seconds (86.80 M allocations: 3.113 GB, 3.67% gc time)
